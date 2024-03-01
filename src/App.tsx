3e76a9e26167e4b900515437fb3e6bc3c3f0f1dd
PhilipeF <philipesferreiraa@gmail.com>
2024-03-01 16:00:52 -0300

    Feat: Dando inicio a criação do componente LastItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-const App = () => {
+import { useState } from 'react';
+import { Area, Container, Header } from './App.styles';
+import { Item } from './types/Item';
+
+export const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Limpar Quintal', done: false },
+    { id: 2, name: 'Lavar Roupa', done: false },
+    { id: 3, name: 'Fazer Comida', done: false }
+  ]);
+
   return (
-    <>
-      Hello Word
-    </>
+    <Container>
+      <Area>
+        <Header>
+          <h1>Lista de Tarefas</h1>
+        </Header>
+        {list.map((item, index) => (
+          <div key={index}>
+            {item.name}
+          </div>
+
+        ))}
+      </Area>
+    </Container>
   )
 }
 
-export default App

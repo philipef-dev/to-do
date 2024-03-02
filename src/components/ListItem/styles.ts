e920496097b439734ec4390cdfce37c97d872568
PhilipeF <philipesferreiraa@gmail.com>
2024-03-02 13:58:54 -0300

    Feat: Dando inicio a estilização do componente ListItem

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
+    background-color: #20212c;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
 `
